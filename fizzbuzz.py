ba1174ae458d7c704c3815bfabca5418152fb5aa
rlaeksqls@naver.com <rlaeksqls@naver.com>
2023-04-24 21:29:46 +0900

    feat: Python env test
    Create do_fizzbuzz and test.:

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+def do_fizzbuzz():
+    """
+    fizznuzz: print fizz buzz fizzbuzz
+    3: fizz
+    5:buzz
+    15: fizzbuzz
+    etc: num
+
+    """
+    print('hello')
+
+
+if __name__=='__main__':
+    do_fizzbuzz()
